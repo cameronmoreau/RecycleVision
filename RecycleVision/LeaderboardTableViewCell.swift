d8e13a45dbb4c75f1479d11b348ca8703d3ca345
maryehuerta <mary.huerta@mavs.uta.edu>
2017-04-22 15:10:55 -0500

    Leaderboard basic thing

---
+++ RecycleVision/LeaderboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderboardTableViewCell.swift
+//  RecycleVision
+//
+//  Created by Mary Huerta on 4/22/17.
+//  Copyright © 2017 Mobi. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
